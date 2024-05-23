3af3cc152619995304bddc5045d53e256f2c3579
kjkj0724 <kjkj0724@gmail.com>
2024-05-23 11:12:58 +0900

    mysql-server change

--- ai/tests.py
+++ ai/tests.py
@@ -1,3 +1,21 @@
+import os
+from pathlib import Path
+
+import joblib
 from django.test import TestCase
 
-# Create your tests here.
+from ai.models import ReplyAi
+
+
+class AiTest(TestCase):
+    model_file_path = os.path.join(Path(__file__).resolve().parent, 'ai/reply_default_model.pkl')
+    model = joblib.load(model_file_path)
+    prediction = model.predict(['안녕'])
+    print(prediction)
+
+    # 추가 fit
+    # transformed_X_train = model.named_steps['count_vectorizer'].transform(['안녕'])
+    # model.named_steps['multinomial_NB'].partial_fit(transformed_X_train, [0])
+    # joblib.dump(model, model_file_path)
+    #
+    # ReplyAi.objects.create(comment='안녕', target=0)
